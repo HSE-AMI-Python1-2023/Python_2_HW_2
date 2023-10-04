a03cb04d290faa2a96f8bb6249eb163ff7cacd03
Palladain <34582887+Palladain@users.noreply.github.com>
2023-10-04 19:40:33 +0300

    Create test_notebook.py

---
+++ test_notebook.py
@@ -0,0 +1,27 @@
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+import os
+import subprocess
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.preprocessors import CellExecutionError
+
+def test_notebook():
+    """
+    Проверяем, что ноутбук запускается, отрабатывает без ошибок
+    """
+
+    with open('./Python_2_HW_1.ipynb') as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=1800, kernel_name='python3', allow_errors=False)
+
+    try:
+        # Check that the notebook runs
+        ep.preprocess(nb, {'metadata': {'path': ''}})
+    except CellExecutionError:
+        raise
+
+    print("Notebook successfully executed`")
